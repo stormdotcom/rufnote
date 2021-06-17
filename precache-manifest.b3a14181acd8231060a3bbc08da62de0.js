cb21ce042bd1bfa59af5ef194837cd3d4eaadc15
unknown <ajmal.nasumudeen@LAP-1677.speridian.com>
2021-06-17 12:55:21 +0530

    Updates

---
+++ precache-manifest.b3a14181acd8231060a3bbc08da62de0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fc511fe5466d801c2f1026994781d9",
+    "url": "/rufnote/index.html"
+  },
+  {
+    "revision": "2f7e6764b638e69e89bf",
+    "url": "/rufnote/static/js/2.48552171.chunk.js"
+  },
+  {
+    "revision": "f1312954f82d074799ad",
+    "url": "/rufnote/static/js/main.072445b1.chunk.js"
+  },
+  {
+    "revision": "bd4d53a99ba515c9f9f8",
+    "url": "/rufnote/static/js/runtime-main.9b628422.js"
+  }
+]);
